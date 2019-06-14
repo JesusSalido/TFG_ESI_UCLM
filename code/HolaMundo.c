2ccf60344277a3c26345181090c37c5fb9ffc490
Jesús Salido Tercero <JesusSalido@users.noreply.github.com>
2019-06-14 10:40:01 +0200

    Initial Overleaf Import

---
+++ code/HolaMundo.c
@@ -0,0 +1,6 @@
+// Este código ha sido incluido desde un 
+// fichero externo (HolaMundo.c)
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+  puts("¡Hola mundo!");
+}
---
+++ code/HolaMundo.java
@@ -0,0 +1,9 @@
+/* 
+   El programa Hola Mundo muestra un saludo en la pantalla
+*/
+public class HolaMundo {
+    public static void main(String[] args){
+      //Muestra "Hola Mundo!"
+      System.out.println("Hola Mundo!");     
+    } 
+}
